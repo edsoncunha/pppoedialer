4548006e9427c116fa4604ef992e8f93db8b35ea
Edson Cunha <senhoredson@gmail.com>
2014-04-16 03:28:02 -0300

    envio inicial

---
+++ CreateAndDialVpnEntry/MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DotRas.Samples.CreateAndDialVpnEntry
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.AllUsersPhoneBook = new DotRas.RasPhoneBook(this.components);
+            this.DialButton = new System.Windows.Forms.Button();
+            this.Dialer = new DotRas.RasDialer(this.components);
+            this.StatusTextBox = new System.Windows.Forms.TextBox();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DialButton
+            // 
+            this.DialButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.DialButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DialButton.Location = new System.Drawing.Point(293, 203);
+            this.DialButton.Name = "DialButton";
+            this.DialButton.Size = new System.Drawing.Size(125, 41);
+            this.DialButton.TabIndex = 1;
+            this.DialButton.Text = "&Conectar";
+            this.DialButton.UseVisualStyleBackColor = true;
+            this.DialButton.Click += new System.EventHandler(this.DialButton_Click);
+            // 
+            // Dialer
+            // 
+// TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+// TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+            this.Dialer.Credentials = null;
+            this.Dialer.EapOptions = new DotRas.RasEapOptions(false, false, false);
+            this.Dialer.HangUpPollingInterval = 0;
+            this.Dialer.Options = new DotRas.RasDialOptions(false, false, false, false, false, false, false, false, false, false, false);
+            this.Dialer.SynchronizingObject = this;
+            this.Dialer.StateChanged += new System.EventHandler<DotRas.StateChangedEventArgs>(this.Dialer_StateChanged);
+            this.Dialer.DialCompleted += new System.EventHandler<DotRas.DialCompletedEventArgs>(this.Dialer_DialCompleted);
+            // 
+            // StatusTextBox
+            // 
+            this.StatusTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.StatusTextBox.Location = new System.Drawing.Point(16, 280);
+            this.StatusTextBox.Multiline = true;
+            this.StatusTextBox.Name = "StatusTextBox";
+            this.StatusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.StatusTextBox.Size = new System.Drawing.Size(402, 145);
+            this.StatusTextBox.TabIndex = 2;
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DisconnectButton.Enabled = false;
+            this.DisconnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisconnectButton.Location = new System.Drawing.Point(167, 203);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(120, 41);
+            this.DisconnectButton.TabIndex = 3;
+            this.DisconnectButton.Text = "D&esconectar";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::DotRas.Samples.CreateAndDialVpnEntry.Properties.Resources.g2;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 89);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Senha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(171, 121);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(167, 23);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(171, 157);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(167, 23);
+            this.txtSenha.TabIndex = 9;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.DialButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 437);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.StatusTextBox);
+            this.Controls.Add(this.DialButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conectar a G2 Telecom";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DotRas.RasPhoneBook AllUsersPhoneBook;
+        private System.Windows.Forms.Button DialButton;
+        private DotRas.RasDialer Dialer;
+        private System.Windows.Forms.TextBox StatusTextBox;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtUsuario;
+    }
+}
+
---
+++ CreateAndDialVpnEntry/MainForm.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using DotRas;
+using System.Diagnostics;
+
+namespace DotRas.Samples.CreateAndDialVpnEntry
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Defines the name of the entry being used by the example.
+        /// </summary>
+        public const string EntryName = "VPN Connection";
+
+        /// <summary>
+        /// Holds a value containing the handle used by the connection that was dialed.
+        /// </summary>
+        private RasHandle handle = null;
+
+        public MainForm()
+        {
+            this.InitializeComponent();
+
+            //var device = RasDevice.GetDeviceByName("(PPPoE)", RasDeviceType.PPPoE);
+
+            //RasEntry.CreateBroadbandEntry("Meu PPPoE", device);
+        }
+
+        /// <summary>
+        /// Occurs when the user clicks the Create Entry button.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
+        private void CreateEntryButton_Click(object sender, EventArgs e)
+        {
+            // This opens the phonebook so it can be used. Different overloads here will determine where the phonebook is opened/created.
+            this.AllUsersPhoneBook.Open();
+
+            // Create the entry that will be used by the dialer to dial the connection. Entries can be created manually, however the static methods on
+            // the RasEntry class shown below contain default information matching that what is set by Windows for each platform.
+            RasEntry entry = RasEntry.CreateVpnEntry(EntryName, IPAddress.Loopback.ToString(), RasVpnStrategy.Default,
+                RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn));
+
+            // Add the new entry to the phone book.
+            this.AllUsersPhoneBook.Entries.Add(entry);
+        }
+
+        /// <summary>
+        /// Occurs when the user clicks the Dial button.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
+        private void DialButton_Click(object sender, EventArgs e)
+        {
+            this.StatusTextBox.Clear();
+
+            // This button will be used to dial the connection.
+            this.Dialer.EntryName = "G2 Telecom";
+            this.Dialer.PhoneBookPath = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User);
+
+            try
+            {
+                // Set the credentials the dialer should use.
+                this.Dialer.Credentials = new NetworkCredential(txtUsuario.Text, txtSenha.Text);
+
+                // NOTE: The entry MUST be in the phone book before the connection can be dialed.
+                // Begin dialing the connection; this will raise events from the dialer instance.
+                this.handle = this.Dialer.DialAsync();
+
+                // Enable the disconnect button for use later.
+                this.DisconnectButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                this.StatusTextBox.AppendText(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Occurs when the dialer state changes during a connection attempt.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An <see cref="DotRas.StateChangedEventArgs"/> containing event data.</param>
+        private void Dialer_StateChanged(object sender, StateChangedEventArgs e)
+        {
+            string texto = "";
+
+            switch (e.State)
+            {
+                case RasConnectionState.AllDevicesConnected:
+                    texto = "Todos os dispositivos conectados";
+                    break;
+
+                case RasConnectionState.ApplySettings:
+                    texto = "Aplicando configurações...";
+                    break;
+
+                case RasConnectionState.AuthAck:
+                    texto = "Ack de autenticação...";
+                    break;
+
+                case RasConnectionState.AuthCallback:
+                    texto = "Resposta de autenticação";
+                    break;
+
+                case RasConnectionState.AuthChangePassword:
+                    texto = "Mudar senha";
+                    break;
+
+                case RasConnectionState.Authenticate:
+                    texto = "Autenticar";
+                    break;
+
+                case RasConnectionState.Authenticated:
+                    texto = "Autenticado...";
+                    break;
+
+                case RasConnectionState.AuthLinkSpeed:
+                    texto = "Velocidade do link";
+                    break;
+
+                case RasConnectionState.AuthNotify:
+                    texto = "Notificação de autenticação";
+                    break;
+
+                case RasConnectionState.AuthProject:
+                    texto = "Autenticação de projeto";
+                    break;
+
+                case RasConnectionState.AuthRetry:
+                    texto = "Nova tentativa de autenticação";
+                    break;
+
+                case RasConnectionState.CallbackComplete:
+                    texto = "Callback concluído";
+                    break;
+
+                case RasConnectionState.CallbackSetByCaller:
+                    texto = "Callback enviado pelo chamador";
+                    break;
+
+                case RasConnectionState.ConnectDevice:
+                    texto = "Conectar dispositivo";
+                    break;
+
+                case RasConnectionState.Connected:
+                    texto = "Conectado";
+                    break;
+
+                case RasConnectionState.DeviceConnected:
+                    texto = "Dispositivo conectado";
+                    break;
+
+                case RasConnectionState.Disconnected:
+                    texto = "Desconectado";
+                    break;
+
+                case RasConnectionState.Interactive:
+                    texto = "Interativo";
+                    break;
+
+                case RasConnectionState.InvokeEapUI:
+                    texto = "Invocar EAP UI";
+                    break;
+
+                case RasConnectionState.LogOnNetwork:
+                    texto = "Autenticar na rede";
+                    break;
+
+                case RasConnectionState.OpenPort:
+                    texto = "Abrir porta";
+                    break;
+
+                case RasConnectionState.PasswordExpired:
+                    texto = "Senha expirada";
+                    break;
+
+                case RasConnectionState.PortOpened:
+                    texto = "Porta aberta";
+                    break;
+
+                case RasConnectionState.PostCallbackAuthentication:
+                    texto = "Autenticação pós-callback";
+                    break;
+
+                case RasConnectionState.PrepareForCallback:
+                    texto = "Preparar para callback";
+                    break;
+
+                case RasConnectionState.Projected:
+                    texto = "Projetado";
+                    break;
+
+                case RasConnectionState.RetryAuthentication:
+                    texto = "Tentar autenticação novamente";
+                    break;
+
+                case RasConnectionState.StartAuthentication:
+                    texto = "Iniciar autenticação";
+                    break;
+
+                case RasConnectionState.SubEntryConnected:
+                    texto = "Sub-entrada conectada";
+                    break;
+
+                case RasConnectionState.SubEntryDisconnected:
+                    texto = "Sub-entrada desconectada";
+                    break;
+
+                case RasConnectionState.WaitForCallback:
+                    texto = "Aguardar callback...";
+                    break;
+
+                case RasConnectionState.WaitForModemReset:
+                    texto = "Aguardar reinício do modem...";
+                    break;
+
+
+            }
+
+
+            this.StatusTextBox.AppendText(texto + "\r\n");
+        }
+
+        /// <summary>
+        /// Occurs when the dialer has completed a dialing operation.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An <see cref="DotRas.DialCompletedEventArgs"/> containing event data.</param>
+        private void Dialer_DialCompleted(object sender, DialCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                this.StatusTextBox.AppendText("Cancelled!");
+            }
+            else if (e.TimedOut)
+            {
+                this.StatusTextBox.AppendText("Connection attempt timed out!");
+            }
+            else if (e.Error != null)
+            {
+                this.StatusTextBox.AppendText(e.Error.ToString());
+            }
+            else if (e.Connected)
+            {
+                this.StatusTextBox.AppendText("Conectado!");
+                Process.Start("http://www.g2telecom.com.br/");
+            }
+
+            if (!e.Connected)
+            {
+                // The connection was not connected, disable the disconnect button.
+                this.DisconnectButton.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Occurs when the user clicks the Disconnect button.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An <see cref="System.EventArgs"/> containing event data.</param>
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            if (this.Dialer.IsBusy)
+            {
+                // The connection attempt has not been completed, cancel the attempt.
+                this.Dialer.DialAsyncCancel();
+            }
+            else
+            {
+                // The connection attempt has completed, attempt to find the connection in the active connections.
+                RasConnection connection = RasConnection.GetActiveConnectionByHandle(this.handle);
+                if (connection != null)
+                {
+                    // The connection has been found, disconnect it.
+                    connection.HangUp();
+                }
+            }
+        }
+    }
+}
